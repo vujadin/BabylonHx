d1f2eb34bbcb7674713fe8f0dcde6d785c79f189
vujadin.krtolica <vujadin@gamestudiohx.com>
2015-11-24 23:23:55 +0100

    Mesh instances sample added

--- src/samples/ForestOfPythagoras.hx
+++ src/samples/ForestOfPythagoras.hx
@@ -1,6 +1,7 @@
 package samples;
 
 import com.babylonhx.Scene;
+import com.babylonhx.Engine;
 import com.babylonhx.mesh.Mesh;
 import com.babylonhx.math.Matrix;
 import com.babylonhx.math.Color3;
@@ -13,13 +14,14 @@ import com.babylonhx.lights.HemisphericLight;
 import com.babylonhx.lights.shadows.ShadowGenerator;
 import com.babylonhx.materials.textures.Texture;
 import com.babylonhx.mesh.VertexBuffer;
-//import com.babylonhx.materials.lib.terrain.TerrainMaterial;
+import com.babylonhx.materials.textures.procedurals.standard.GrassProceduralTexture;
+import com.babylonhx.materials.textures.procedurals.standard.WoodProceduralTexture;
 
 /**
  * ...
  * @author Krtolica Vujadin
  */
-class PythagorianThrees {
+class ForestOfPythagoras {
 	
 	var colorScale:Array<Int> = [
 		0xfff7f3, 0xfde0dd, 0xfcc5c0,
@@ -32,41 +34,100 @@ class PythagorianThrees {
 
 	public function new(scene:Scene) {
 		this.scene = scene;
+		scene.clearColor = Color3.Black();
 		
-		var camera = new ArcRotateCamera("Camera", Math.PI / 3, Math.PI / 2, 8, new Vector3(0, 1.5, 0), scene);
+		var camera = new ArcRotateCamera("Camera", 4, 1.43, 26, new Vector3(0, 1.5, 0), scene);
 		camera.attachControl();
-				
-		var hemiLight = new HemisphericLight("hemi", new Vector3(0, 1, 0), scene);
-		hemiLight.diffuse = Color3.FromInt(0x50505a);
-		hemiLight.intensity = 0.6;
 		
-		var light = new SpotLight("*spot00", new Vector3(-0.2, 10, -10), new Vector3(0, -1, 1.0), 1.2, 3.1, scene);
-		light.intensity = 0.7;
+		Engine.mouseUp.push(function ()
+		{
+			trace(camera.alpha, camera.beta, camera.radius);
+		});
+						
+		var name = "wood";		
+		
+		var woodMaterial = new StandardMaterial(name, scene);
+		var woodTexture = new WoodProceduralTexture(name + "tex", 512, scene);
+		woodTexture.ampScale = 20.0;
+		woodMaterial.diffuseTexture = woodTexture;
+		woodMaterial.specularColor = Color3.Black();
+		
+		name = "grass";
+		
+		var grassMaterial = new StandardMaterial(name, scene);
+		var grassTexture = new GrassProceduralTexture(name + "tex", 1024, scene);
+		grassMaterial.ambientTexture = grassTexture;
+		grassMaterial.specularColor = new Color3(0.2, 0.2, 0.2);
+		
+		var light = new SpotLight("spotlight", new Vector3(-0.2, 50, -55), new Vector3(0, -1, 1.0), 1.2, 2.8, scene);
+		light.intensity = 0.5;
 		light.diffuse = Color3.FromInt(0xffffdd);
 		
-		var ground = Mesh.CreateGround("ground1", 50, 50, 2, scene);
+		var ground = Mesh.CreateGround("ground1", 100, 100, 20, scene);
+		ground.material = grassMaterial;
 		ground.position.y = -0.5;
+		ground.position.z = 15;
 		ground.receiveShadows = true;
 					
 		createTreeGeometry();
 		
 		var m = Mesh.MergeMeshes(meshes, true, true);
+		m.material = woodMaterial;
+		cast(m.material, StandardMaterial).checkReadyOnlyOnce = true;
+		cast(m.material, StandardMaterial).checkReadyOnEveryCall = false;
+		m.position.x = -5;
 		m.receiveShadows = true;
-		var m2 = m.clone("clone");
-		m2.position.x = 10;
+		var m2 = m.createInstance("clone");
+		m2.position.x = 5;
 		m2.rotation.y = Math.PI / 5;
-		m2.scaling.set(2.3, 2.3, 2.3);
+		m2.scaling.set(2.1, 2.1, 2.1);
 		m2.receiveShadows = true;
 		
-		var finalMesh = Mesh.MergeMeshes([m, m2], true, true);
-		finalMesh.position.x -= 5;
-		cast(finalMesh.material, StandardMaterial).diffuseColor = Color3.FromInt(0xff9f42);
-		cast(finalMesh.material, StandardMaterial).checkReadyOnlyOnce = true;
-		cast(finalMesh.material, StandardMaterial).checkReadyOnEveryCall = false;
+		var m3 = m.createInstance("clone2");
+		m3.position.x = -2;
+		m3.position.z = 10;
+		m3.rotation.y = Math.PI / 3;
+		m3.scaling.set(1.3, 1.3, 1.3);
+		m3.receiveShadows = true;
 		
+		var m4 = m.createInstance("clone3");
+		m4.position.x = -2;
+		m4.position.z = -15;
+		m4.rotation.y = Math.PI / 2.5;
+		m4.scaling.set(1.6, 1.6, 1.6);
+		m4.receiveShadows = true;
+		
+		var m5 = m.createInstance("clone4");
+		m5.position.x = -18;
+		m5.position.z = -10;
+		m5.rotation.y = Math.PI;
+		m5.scaling.set(1.9, 1.9, 1.9);
+		m5.receiveShadows = true;
+		
+		var m6 = m.createInstance("clone5");
+		m6.position.x = 15;
+		m6.position.z = 7;
+		m6.rotation.y = Math.PI;
+		m6.scaling.set(1.4, 1.4, 1.4);
+		m6.receiveShadows = true;
+		
+		var m7 = m.createInstance("clone6");
+		m7.position.x = -15;
+		m7.position.z = 10;
+		m7.rotation.y = Math.PI * 0.8;
+		m7.scaling.set(2.8, 2.8, 2.8);
+		m7.receiveShadows = true;
+				
 		var shadowGenerator = new ShadowGenerator(1024, light);
-		shadowGenerator.getShadowMap().renderList.push(finalMesh);
+		shadowGenerator.getShadowMap().renderList.push(m);
+		shadowGenerator.getShadowMap().renderList.push(m2);
+		shadowGenerator.getShadowMap().renderList.push(m3);
+		shadowGenerator.getShadowMap().renderList.push(m4);
+		shadowGenerator.getShadowMap().renderList.push(m5);
+		shadowGenerator.getShadowMap().renderList.push(m6);
+		shadowGenerator.getShadowMap().renderList.push(m7);
 		shadowGenerator.usePoissonSampling = true; 
+		shadowGenerator.getShadowMap().refreshRate = 0;
 				
 		scene.getEngine().runRenderLoop(function () {
 			scene.render();
