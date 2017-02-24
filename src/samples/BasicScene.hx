2fbcccef31a05334c011201b95e8bdbe5f3f6047
Krtolica Vujadin <vuja22@gmail.com>
2017-02-24 14:30:38 +0100

    Update BasicScene.hx

--- src/samples/BasicScene.hx
+++ src/samples/BasicScene.hx
@@ -22,15 +22,6 @@ import com.babylonhx.collisions.PickingInfo;
 
 import com.babylonhx.tools.EventState;
 import com.babylonhx.utils.Image;
-import com.babylonhx.materials.textures.RawTexture;
-import com.babylonhx.materials.textures.procedurals.standard.PlasmaProceduralTexture;
-import com.babylonhx.materials.textures.procedurals.standard.SpiralProceduralTexture;
-import com.babylonhx.materials.textures.procedurals.standard.CombustionProceduralTexture;
-import com.babylonhx.materials.textures.procedurals.standard.ElectricProceduralTexture;
-import com.babylonhx.materials.textures.procedurals.standard.VoronoiProceduralTexture;
-
-import com.babylonhx.postprocess.NotebookDrawingsPostProcess;
-import com.babylonhx.postprocess.WatercolorPostProcess;
 
 /**
  * ...
@@ -39,7 +30,6 @@ import com.babylonhx.postprocess.WatercolorPostProcess;
 class BasicScene {
 
 	public function new(scene:Scene) {
-		//var camera = new WebVRFreeCamera("camera1", new Vector3(0, 5, -10), scene);
 		var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
 		camera.setTarget(Vector3.Zero());
 		camera.attachControl();
@@ -48,34 +38,16 @@ class BasicScene {
 		light.intensity = 1;
 		
 		var sphere = Mesh.CreateSphere("sphere1", 16, 2, scene);
-		/*sphere.material = new StandardMaterial("mat", scene);
-		untyped sphere.material.diffuseColor = new Color3(0.3, 0.34, 0.87);*/
 		sphere.position.y = 1;
 		
 		var ground = Mesh.CreateGround("ground1", 26, 26, 2, scene);
-		
-		/*var plasmaMaterial = new StandardMaterial("mat", scene);
-		var plasmaTexture = new PlasmaProceduralTexture("plasma", 512, scene);
-		plasmaMaterial.diffuseTexture = plasmaTexture;
-		
-		sphere.material = ground.material = plasmaMaterial;*/
-		
-		/*var spiralMaterial = new StandardMaterial("mat", scene);
-		var spiralTexture = new VoronoiProceduralTexture("spiral", 512, scene);
-		spiralMaterial.diffuseTexture = spiralTexture;		
-		sphere.material = ground.material = spiralMaterial;*/
-		
-		//var ndPP = new NotebookDrawingsPostProcess("notebookDrawings_PP", 1.0, camera);
-		var wPP = new WatercolorPostProcess("watercolor_PP", 1.0, camera);
-		
+				
 		var rotationAxis = new Vector3(1, 0, 0);
 		var rotationAngle = 0.0;
 		var rotationSpeed = 0.003;
 		
 		scene.getEngine().runRenderLoop(function () {
             scene.render();
-			//sphere.rotate(rotationAxis, rotationAngle * rotationSpeed, Space.WORLD);
-			//rotationAngle += 0.005;
         });
 	}
 	
